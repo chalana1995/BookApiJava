74a34c9303e7a923b244111106e9d968182e486e
chalana kalpitha <chalanakalpitha72@gmail.com>
2024-06-24 19:39:42 +0530

    initial commit

---
+++ src/main/java/ck/project/bookingApiProject/BookingApiProjectApplication.java
@@ -0,0 +1,13 @@
+package ck.project.bookingApiProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingApiProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingApiProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ck/project/bookingApiProject/BookingApiProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ck.project.bookingApiProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingApiProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
